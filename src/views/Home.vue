1a8e97046f9db766814b713927b28b436daecf04
Raku Zeta <zetaraku@gmail.com>
2019-12-31 00:30:05 +0800

    edit views/Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Welcome to mai-tools</h1>
+    <p>
+      <b>mai-tools</b> is a collection of useful tools for <b>maimai</b> and <b>maimai DX</b>.
+    </p>
+    <h3>Score Calculator</h3>
+    <ul>
+      <b>Score Calculator</b> is an utility that can be used to calculate score and achievement.
+    </ul>
+    <h3>More utils</h3>
+    <span>Coming soon...</span>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
-  }
+  components: {},
 }
 </script>
+
+<style lang="scss" scoped>
+h3 {
+  margin: 40px 0 20px 0;
+}
+</style>
