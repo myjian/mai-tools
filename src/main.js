1576e22b999a4d899d46b5f55c02fd2474bf1fcf
Raku Zeta <zetaraku@gmail.com>
2019-12-31 13:14:17 +0800

    add vue-gtag

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import VueGtag from "vue-gtag";
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(VueGtag, {
+  config: { id: "UA-83813049-2" },
+}, router);   // Enable auto tracking
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: h => h(App),
+}).$mount('#app');
