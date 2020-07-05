2424f598cc4baf343ab39f36f9a96a281fce465d
Ming-Yuan Jian <mingyuan@dropbox.com>
2020-07-06 05:39:17 +0800

    further shorten score converter url

--- scripts/score-converter.js
+++ scripts/score-converter.js
@@ -1,12 +1,19 @@
 "use strict";
 (function (d) {
   function trimSpaces(textLine) {
-    return textLine.trim().replace(/\t/g, " ");
+    return textLine.trim().replace(/\t/g, "-");
   }
   if ((d.location.host === "maimaidx-eng.com" || d.location.host === "maimaidx.jp") && d.location.pathname.includes("/maimai-mobile/record/playlogDetail/")) {
     var st = d.querySelector(".basic_block.break").childNodes[1].nodeValue;
     var ac = d.querySelector(".playlog_achievement_txt").innerText;
     var nd = d.querySelector(".playlog_notes_detail").innerText.split("\n").slice(-5).map(trimSpaces).join("\n");
-    window.open("https://myjian.github.io/mai-tools/?st=" + encodeURIComponent(st) + "&ac=" + encodeURIComponent(ac) + "&nd=" + encodeURIComponent(nd), "_blank");
+    window.open(
+      "https://myjian.github.io/mai-tools/?st="
+      + encodeURIComponent(st)
+      + "&ac="
+      + encodeURIComponent(ac)
+      + "&nd="
+      + encodeURIComponent(nd),
+      "_blank");
   }
 })(document);
