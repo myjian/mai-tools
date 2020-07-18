24c5dd0273bb02a8e47cdb9be7953d2902c295db
Ming-Yuan Jian <mingyuan@dropbox.com>
2020-07-18 08:00:02 +0800

    [Rating calc] Adopt webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,36 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
   mode: "production",
-  entry: './classic-layout/src/main.tsx',
+  entry: {
+    "classic-layout": "./classic-layout/src/main.tsx",
+    "rating-calculator": "./rating-calculator/src/main.js",
+  },
   output: {
-    path: path.resolve(__dirname, 'classic-layout/dist'),
-    filename: 'main.bundle.js'
+    path: __dirname,
+    filename: (pathData) => {
+      switch (pathData.chunk.name) {
+        case "classic-layout":
+          return "classic-layout/dist/main.bundle.js";
+        case "rating-calculator":
+          return "rating-calculator/dist/main.bundle.js";
+      }
+    },
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         exclude: /node_modules/,
-        use: "ts-loader"
-      }
-    ]
+        use: "ts-loader",
+      },
+    ],
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"]
+    extensions: [".tsx", ".ts", ".js"],
   },
   externals: {
-    "react": "React",
-    "react-dom": "ReactDOM"
-  }
-}
+    react: "React",
+    "react-dom": "ReactDOM",
+  },
+};
