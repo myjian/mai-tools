7f4ab0ea1fa44e500373782d59c0aaeb08126586
Ming-Yuan Jian <mingyuan@dropbox.com>
2020-07-28 14:20:08 +0800

    [Bookmarklets] Change page tile on context menu click or touch link

--- js/bookmarklets/BookmarkItem.tsx
+++ js/bookmarklets/BookmarkItem.tsx
@@ -8,23 +8,30 @@ export const BookmarkItem: React.FC<Bookmarklet> = ({
   howTo,
   screenshotUrl,
   scriptUrl,
-}) => (
-  <React.Fragment>
-    <div className="bookmarklet">
-      <div className="bookmarkletText">
-        <h3 className="bookmarkletTitle">
-          <a href={scriptUrl}>{itemTitle}</a>
-        </h3>
-        <ul>
-          <li>{feature}</li>
-          <li>{typeof howTo === "string" ? howTo : howTo()}</li>
-        </ul>
+}) => {
+  const setPageTitle = () => {
+    document.title = itemTitle;
+  };
+  return (
+    <React.Fragment>
+      <div className="bookmarklet">
+        <div className="bookmarkletText">
+          <h3 className="bookmarkletTitle">
+            <a href={scriptUrl} onTouchStart={setPageTitle} onContextMenu={setPageTitle}>
+              {itemTitle}
+            </a>
+          </h3>
+          <ul>
+            <li>{feature}</li>
+            <li>{typeof howTo === "string" ? howTo : howTo()}</li>
+          </ul>
+        </div>
+        <div className="bookmarkletImage">
+          <a href={screenshotUrl}>
+            <img className="screenshot" alt="screenshot" src={screenshotUrl} />
+          </a>
+        </div>
       </div>
-      <div className="bookmarkletImage">
-        <a href={screenshotUrl}>
-          <img className="screenshot" alt="screenshot" src={screenshotUrl} />
-        </a>
-      </div>
-    </div>
-  </React.Fragment>
-);
+    </React.Fragment>
+  );
+};
