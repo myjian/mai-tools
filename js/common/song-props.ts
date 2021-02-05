2bb977050351ea368e224e8093e7d2f2828075d4
Ming-Yuan Jian <mingyuan@dropbox.com>
2021-02-05 09:08:04 -0800

    Fix song name ≠彡"/了→

--- js/common/song-props.ts
+++ js/common/song-props.ts
@@ -18,13 +18,7 @@ function fixMismatchSongName(name: string) {
   if (name === "D✪N’T  ST✪P  R✪CKIN’") {
     return "D✪N’T ST✪P R✪CKIN’";
   }
-  if (name === `Little " + '"' + "Sister" + '"' + " Bitch`) {
-    return `Little "Sister" Bitch`;
-  }
-  if (name === `Sqlupp (Camellia's " + '"' + "Sqleipd*Hiytex" + '"' + " Remix)`) {
-    return `Sqlupp (Camellia's "Sqleipd*Hiytex" Remix)`;
-  }
-  return name;
+  return name.replace(/" \+ '/g, "").replace(/' \+ "/g, "");
 }
 
 /**
