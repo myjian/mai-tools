8b2a9bf3bc44eeb1d1da7d8c16031ea7ca511d49
Ming-Yuan Jian <mingyuan@dropbox.com>
2021-08-25 00:14:37 -0700

    [Album download helper] Add link to song name too

--- scripts/album-download-helper.ts
+++ scripts/album-download-helper.ts
@@ -1,5 +1,6 @@
 (function (d) {
   const DIFF_REGEX = /music_(\w+)_score_back/;
+  const DOWNLOAD_ICON = "⭳";
   // 540 = 9 * 60 minutes = UTC+9 (Japan Time), 1 minute = 60000 milliseconds
   const timezoneOffset = (540 + new Date().getTimezoneOffset()) * 60000;
 
@@ -37,25 +38,43 @@
 
   function getFileName(row: HTMLElement) {
     const playDate = getPlayDate(row);
-    const songName = (row.getElementsByClassName(
-      "black_block"
-    )[0] as HTMLElement).innerText.replace(/<>:"\/\\\|\?\*/g, "-");
+    const songName = (
+      row.getElementsByClassName("black_block")[0] as HTMLElement
+    ).innerText.replace(/<>:"\/\\\|\?\*/g, "-");
     const difficulty = row.className.match(DIFF_REGEX);
     return difficulty
       ? `${formatDate(playDate)} ${songName} ${difficulty[1].toUpperCase()}.jpg`
       : `${formatDate(playDate)} ${songName}.jpg`;
   }
 
-  function assignFilenameToPhoto(row: HTMLElement) {
+  function getPhotoLink(row: HTMLElement) {
+    const img = row.querySelector("img.w_430") as HTMLImageElement;
+    return img.src;
+  }
+
+  function addLinkToPhoto(row: HTMLElement, href: string, filename: string) {
     if (row.getElementsByTagName("a").length) {
       return;
     }
     const img = row.querySelector("img.w_430") as HTMLImageElement;
     const link = d.createElement("a");
-    link.download = getFileName(row);
-    link.href = img.src;
-    img.insertAdjacentElement('beforebegin', link);
+    link.download = filename;
+    link.href = href;
+    img.insertAdjacentElement("beforebegin", link);
     link.append(img);
+  }
+
+  function addLinkToSongname(row: HTMLElement, href: string, filename: string) {
+    const songnameBlock = row.getElementsByClassName("black_block")[0];
+    if (songnameBlock.getElementsByTagName("a").length) {
+      return;
+    }
+    const link = d.createElement("a");
+    link.download = filename;
+    link.href = href;
+    link.target = "_blank";
+    songnameBlock.append(link);
+    link.append(songnameBlock.childNodes[0], " ", DOWNLOAD_ICON);
   }
 
   function main() {
@@ -63,7 +82,10 @@
     document.body.oncontextmenu = null;
     const rows = Array.from(d.getElementsByClassName("black_block")).map((r) => r.parentElement);
     for (const row of rows) {
-      assignFilenameToPhoto(row);
+      const photoLink = getPhotoLink(row);
+      const filename = getFileName(row);
+      addLinkToPhoto(row, photoLink, filename);
+      addLinkToSongname(row, photoLink, filename);
     }
   }
   main();
