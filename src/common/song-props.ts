6322651f0d9e36d571e3ff4e13f1e076292ef21c
Ming-yuen Jien <myjian@users.noreply.github.com>
2023-02-06 18:27:35 +0900

    Add version override to 3 songs
    BREaK! BREaK! BREaK!
    自傷無色
    劣等上等
    
    Fixes #36

--- src/common/song-props.ts
+++ src/common/song-props.ts
@@ -30,9 +30,20 @@ const VERSION_REGEX = /\bv\s*:\s*(-?[0-9]+)/;
 const SONGNAME_REGEX = /\bn\s*:\s*["'](.+?)['"]\s*[,\}]/;
 const SONGNICKNAME_REGEX = /\bnn\s*:\s*["'](.+?)['"]\s*[,\}]/;
 
+// INTL_VER_SONG_PROPS contains songs whose International version is older than
+// JP version.
 const INTL_VER_SONG_PROPS: ReadonlyArray<SongProperties> = [
-  // 宿星審判 debut is SPLASH PLUS (intl), UNiVERSE (Jp)
+  // BREaK! BREaK! BREaK! is debuted at SPLASH (intl), SPLASH PLUS (Jp)
+  {name: "BREaK! BREaK! BREaK!", dx: 1, debut: 15, lv: [-5, -8, 12.8, 14.7, 0]},
+  // 宿星審判 is debuted at SPLASH PLUS (intl), UNiVERSE (Jp)
   {name: "宿星審判", dx: 1, debut: 16, lv: [-4, -8, -12, 14.4, 0]},
+];
+
+const INTL_VER_OVERRIDES: ReadonlyArray<Partial<SongProperties>> = [
+  // 自傷無色 is debuted at Festival (intl), SPLASH (Jp)
+  {name: "自傷無色", dx: 1, debut: 19},
+  // 劣等上等 is debuted at Festival (intl), SPLASH (Jp)
+  {name: "劣等上等", dx: 1, debut: 19},
 ];
 
 const OVERRIDES_BY_VERSION = new Map<DxVersion, ReadonlyArray<Partial<SongProperties>>>([
@@ -125,6 +136,9 @@ export function buildSongPropsMap(gameVer: DxVersion, gameRegion: GameRegion, te
   if (gameRegion === GameRegion.Intl) {
     for (const songProps of INTL_VER_SONG_PROPS) {
       insertOrUpdateSongProps(songPropsByName, songProps);
+    }
+    for (const songProps of INTL_VER_OVERRIDES) {
+      updateSongProps(songPropsByName, songProps);
     }
   }
   const overrides = OVERRIDES_BY_VERSION.get(gameVer);
