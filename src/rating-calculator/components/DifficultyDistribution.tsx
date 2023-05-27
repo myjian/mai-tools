c7686b60d4d6198d4d0c4d859cf5d53a0a5615a8
Ming-yuen Jien <myjian@users.noreply.github.com>
2023-05-28 00:23:30 +0900

    Rating calculator: show chart type distribution
    Fixes #58.

--- src/rating-calculator/components/DifficultyDistribution.tsx
+++ src/rating-calculator/components/DifficultyDistribution.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import {ChartRecord} from '../../common/chart-record';
+import {ChartType, getChartTypeName} from '../../common/chart-type';
 import {
   DIFFICULTIES,
   Difficulty,
@@ -16,12 +17,12 @@ const DIFF_RANK_CELL_BASE_CLASSNAME = 'diffRankCell';
 const DIFF_RANK_TOP_LEFT_CELL_CLASSNAME = 'difficultyRankDistHead';
 
 function getRecordsPerDifficulty(records: ReadonlyArray<ChartRecord>) {
-  const recordsPerDifficulty = new Map<Difficulty, ChartRecord[]>();
+  const recordsPerDifficulty = new Map<Difficulty, Record<ChartType, number>>();
   for (let i = DIFFICULTIES.length - 1; i >= 0; i--) {
-    recordsPerDifficulty.set(DIFFICULTIES[i], []);
+    recordsPerDifficulty.set(DIFFICULTIES[i], {[ChartType.DX]: 0, [ChartType.STANDARD]: 0});
   }
   for (const r of records) {
-    recordsPerDifficulty.get(r.difficulty).push(r);
+    recordsPerDifficulty.get(r.difficulty)[r.chartType] += 1;
   }
   return recordsPerDifficulty;
 }
@@ -36,6 +37,24 @@ export const DifficultyDistribution = ({chartRecords, topChartsCount}: Props) =>
   const topRecords = chartRecords.slice(0, topChartsCount);
   const recordsPerDiff = getRecordsPerDifficulty(topRecords);
   const keyMap = new Map<string, boolean>([[CommonMessages[lang].subtotal, true]]);
+  const rows: JSX.Element[] = [];
+  recordsPerDiff.forEach((countByChartType, d) => {
+    for (const chartType of [ChartType.DX, ChartType.STANDARD]) {
+      if (!countByChartType[chartType]) continue;
+      const dist = new Map([[CommonMessages[lang].subtotal, countByChartType[chartType]]]);
+      rows.push(
+        <RankDistributionDataRow
+          key={'' + d + chartType}
+          rowHead={`${getDifficultyName(d)} (${getChartTypeName(chartType)})`}
+          columns={keyMap.keys()}
+          rankDist={dist}
+          rowClassname={DIFFICULTY_CLASSNAME_MAP.get(d)}
+          baseCellClassname={DIFF_RANK_CELL_BASE_CLASSNAME}
+          perColumnClassnames={[]}
+        />
+      );
+    }
+  });
   return (
     <table className="rankDistributionTable">
       <thead>
@@ -46,25 +65,7 @@ export const DifficultyDistribution = ({chartRecords, topChartsCount}: Props) =>
           columns={keyMap.keys()}
         />
       </thead>
-      <tbody>
-        {Array.from(recordsPerDiff.entries()).map(([d, records]) => {
-          if (!records.length) {
-            return;
-          }
-          const dist = new Map<string, number>([[CommonMessages[lang].subtotal, records.length]]);
-          return (
-            <RankDistributionDataRow
-              key={d}
-              rowHead={getDifficultyName(d)}
-              columns={keyMap.keys()}
-              rankDist={dist}
-              rowClassname={DIFFICULTY_CLASSNAME_MAP.get(d)}
-              baseCellClassname={DIFF_RANK_CELL_BASE_CLASSNAME}
-              perColumnClassnames={[]}
-            />
-          );
-        })}
-      </tbody>
+      <tbody>{rows}</tbody>
     </table>
   );
 };
