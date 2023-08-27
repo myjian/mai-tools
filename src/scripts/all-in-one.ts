039f1a8d945d8edba4eec0e3ec1581b72d34b949
Ming-yuan <1978764+myjian@users.noreply.github.com>
2023-08-28 03:26:09 +0900

    Use import in all-in-one to load scripts
    This will make it easier to publish browser extensions
    (if we ever decide to do so)

--- src/scripts/all-in-one.ts
+++ src/scripts/all-in-one.ts
@@ -1,7 +1,6 @@
 import {isMaimaiNetOrigin} from '../common/game-region';
 import {getInitialLanguage, Language} from '../common/lang';
 import {removeScrollControl} from '../common/net-helpers';
-import {getScriptHost} from '../common/script-host';
 import {handleError} from '../common/util';
 
 (function (d) {
@@ -17,49 +16,45 @@ import {handleError} from '../common/util';
       pleaseLogIn: 'maimai DX NET에 로그인 해 주세요.',
     },
   }[LANG];
-  const SCRIPT_HOST = getScriptHost('all-in-one') + '/scripts/';
 
   if (!isMaimaiNetOrigin(d.location.origin)) {
     handleError(UIString.pleaseLogIn);
     return;
   }
 
-  function appendScript(filename: string) {
-    const s = d.createElement('script');
-    s.src = SCRIPT_HOST + filename + '?t=' + Math.floor(Date.now() / 60000);
-    d.body.append(s);
-  }
+  // Enable right click
+  d.body.oncontextmenu = null;
 
   const path = d.location.pathname;
   if (path === '/maimai-mobile/record/') {
-    appendScript('recent-play-downloader.js');
+    import('./recent-play-downloader');
   } else if (path.indexOf('/maimai-mobile/record/playlogDetail/') >= 0) {
-    appendScript('score-converter.js');
-    appendScript('play-record-helper.js');
+    import('./score-converter');
+    import('./play-record-helper');
   } else if (path.indexOf('/maimai-mobile/record/musicDetail/') >= 0) {
     removeScrollControl(d);
-    appendScript('song-detail-helper.js');
+    import('./song-detail-helper');
   } else if (path.indexOf('/maimai-mobile/record/music') >= 0) {
-    appendScript('score-sort.js');
+    import('./score-sort');
   } else if (path.indexOf('/maimai-mobile/friend/') >= 0) {
-    appendScript('analyze-friend-rating-in-new-tab.js');
+    import('./analyze-friend-rating-in-new-tab');
     if (path.indexOf('/maimai-mobile/friend/friendDetail/') >= 0) {
-      appendScript('score-download.js');
+      import('./score-download');
     }
     if (
       path.indexOf('/maimai-mobile/friend/friendGenreVs/battleStart/') >= 0 ||
       path.indexOf('/maimai-mobile/friend/friendLevelVs/battleStart/') >= 0
     ) {
-      appendScript('score-sort.js');
+      import('./score-sort');
     }
   } else if (
     path.indexOf('/maimai-mobile/home/') >= 0 ||
     path.indexOf('/maimai-mobile/playerData/') >= 0
   ) {
     removeScrollControl(d);
-    appendScript('score-download.js');
-    appendScript('analyze-rating-in-newtab.js');
+    import('./score-download');
+    import('./analyze-rating-in-newtab');
   } else if (path.indexOf('/maimai-mobile/photo/') >= 0) {
-    appendScript('album-download-helper.js');
+    import('./album-download-helper');
   }
 })(document);
