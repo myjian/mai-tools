8abc00f893c9774d0aa1ca92576ad248834f0696
Ming-yuan <1978764+myjian@users.noreply.github.com>
2023-12-03 03:52:26 +0900

    [Index Page] Re-order bookmarklets
    Put rating analysis first

--- src/index-page/Bookmarklets.tsx
+++ src/index-page/Bookmarklets.tsx
@@ -2,15 +2,7 @@ import React from 'react';
 
 import {Language} from '../common/lang';
 import {useLanguage} from '../common/lang-react';
-import {
-  albumDownloadHelper,
-  analyzeFriendRating,
-  ratingAnalyzer,
-  recentPlaySummary,
-  scoreConverter,
-  scoreDownloader,
-  scoreSorter,
-} from './all-bookmarklets';
+import {ALL_BOOKMARKLETS} from './all-bookmarklets';
 import {BookmarkItem} from './BookmarkItem';
 import {MobileCreateManual} from './MobileCreateManual';
 import {MobileUseManual} from './MobileUseManual';
@@ -18,25 +10,25 @@ import {PCManual} from './PCManual';
 
 const UIMessages = {
   [Language.zh_TW]: {
-    title: "maimai 書籤小工具介紹與設定教學",
-    intro1: "如果想在 maimai DX NET 使用以下功能，請參照 ",
-    howto: "使用教學",
-    intro2: " 設定書籤小工具。",
-    features: "功能介紹",
+    title: 'maimai 書籤小工具介紹與設定教學',
+    intro1: '如果想在 maimai DX NET 使用以下功能，請參照 ',
+    howto: '使用教學',
+    intro2: ' 設定書籤小工具。',
+    features: '功能介紹',
   },
   [Language.en_US]: {
-    title: "maimai Bookmarklets (Features & How to use)",
-    intro1: "If you want to use the following features, follow ",
-    howto: "Instructions",
-    intro2: " to set up the bookmarklet.",
-    features: "Features",
+    title: 'maimai Bookmarklets (Features & How to use)',
+    intro1: 'If you want to use the following features, follow ',
+    howto: 'Instructions',
+    intro2: ' to set up the bookmarklet.',
+    features: 'Features',
   },
   [Language.ko_KR]: {
-    title: "maimai 책갈피 (기능 및 사용법)",
-    intro1: "maimai DX NET 에서 해당 기능을 사용하고 싶으면 아래 적힌 ",
-    howto: "사용법",
-    intro2: "을 따라주세요.",
-    features: "기능",
+    title: 'maimai 책갈피 (기능 및 사용법)',
+    intro1: 'maimai DX NET 에서 해당 기능을 사용하고 싶으면 아래 적힌 ',
+    howto: '사용법',
+    intro2: '을 따라주세요.',
+    features: '기능',
   },
 };
 
@@ -53,13 +45,9 @@ export const Bookmarklets = () => {
       </p>
 
       <div className="bookmarkletList">
-        <BookmarkItem {...recentPlaySummary} />
-        <BookmarkItem {...albumDownloadHelper} />
-        <BookmarkItem {...ratingAnalyzer} />
-        <BookmarkItem {...analyzeFriendRating} />
-        <BookmarkItem {...scoreSorter} />
-        <BookmarkItem {...scoreConverter} />
-        <BookmarkItem {...scoreDownloader} />
+        {ALL_BOOKMARKLETS.map((bookmarklet, idx) => (
+          <BookmarkItem key={idx} {...bookmarklet} />
+        ))}
         <br />
         <div>
           <h2 id="howto">{messages.howto}</h2>
--- src/index-page/all-bookmarklets.tsx
+++ src/index-page/all-bookmarklets.tsx
@@ -30,7 +30,7 @@ export interface Bookmarklet {
   screenshotUrl: string;
 }
 
-export const scoreConverter: Bookmarklet = {
+const convertDxToFinale: Bookmarklet = {
   id: 'convertDxToFinale',
   itemTitleByLang: {
     [Language.zh_TW]: '換算成舊版達成率 & 分析',
@@ -48,14 +48,14 @@ export const scoreConverter: Bookmarklet = {
     [Language.zh_TW]:
       '使用方式：登入 maimai NET，點選最近遊玩的其中一筆紀錄後執行。執行後會開啟新分頁，顯示舊版成績以及相關數據。點擊地點（Cafe MiLK）可切換計分方式，點擊達成率可切換詳細顯示。',
     [Language.en_US]:
-      'Usage: Log in to maimai NET. Open a recent song record and execute the bookmarklet. New tab will open and display score in old achievement system. You can click on "Cafe MiLK" to switch to DX achievement, and click on the achievement % to see how much percentage was lost per note type.',
+      'Usage: Log in to maimai NET. Open a recent song record and run the bookmarklet. New tab will open and display score in old achievement system. You can click on "Cafe MiLK" to switch to DX achievement, and click on the achievement % to see how much percentage was lost per note type.',
     [Language.ko_KR]:
       '사용법: maimai NET에 로그인하고 최근 플레이한 노래 목록을 연 뒤 북마크를 실행시킵니다. 그러면 새 탭에서 점수와 정확도가 표시 될 것입니다. "Cafe MiLK"를 클릭하여 DX정확도로 변경할 수 있고, 정확도%를 클릭하여 노트 타입별 손실률을 확인할 수 있습니다.',
   },
   screenshotUrl: './screenshots/convert-to-finale-score-20200718.jpg',
 };
 
-export const scoreSorter: Bookmarklet = {
+const scoreSorter: Bookmarklet = {
   id: 'scoreSorter',
   itemTitleByLang: {
     [Language.zh_TW]: '排序成績',
@@ -73,14 +73,14 @@ export const scoreSorter: Bookmarklet = {
     [Language.zh_TW]:
       '使用方式：如果要排序自己的成績，請先到於查詢成績頁，先按照自己想要的篩選方式（曲風、等級、遊戲版本等等）列出成績，再執行書籤。如果是要排序朋友的成績，請先使用對戰功能，按照自己想要的篩選方式（曲風、等級）列出對戰結果，再執行書籤。執行完後畫面上會出現選單，可按喜歡的方式排序。',
     [Language.en_US]:
-      "Usage: For sorting own scores, open historical scores (by genre, level, song title, version, etc.) and execute the bookmarklet. For sorting friend's scores, use Friend VS feature to list scores (by genre or level), and then execute the bookmarklet.",
+      "Usage: For sorting own scores, open historical scores (by genre, level, song title, version, etc.) and run the bookmarklet. For sorting friend's scores, use Friend VS feature to list scores (by genre or level), and then run the bookmarklet.",
     [Language.ko_KR]:
       '사용법: 기록을 정렬하려면 플레이 기록 페이지를 열고 북마크를 실행하세요. 친구의 기록을 정렬하기 위해서는 Friend VS 화면에 들어간 후 북마크를 실행하세요.',
   },
   screenshotUrl: './screenshots/score-sort-20200630.png',
 };
 
-export const recentPlaySummary: Bookmarklet = {
+const recentPlaySummary: Bookmarklet = {
   id: 'recentPlaySummary',
   itemTitleByLang: {
     [Language.zh_TW]: '整理最近遊玩紀錄',
@@ -95,13 +95,13 @@ export const recentPlaySummary: Bookmarklet = {
   howToByLang: {
     [Language.zh_TW]:
       '使用方式：於最近成績列表執行。執行後會在頁面中產生表格，可以選取日期和排序。',
-    [Language.en_US]: 'Usage: Open the recent game records list and execute the bookmarklet.',
+    [Language.en_US]: 'Usage: Open the recent game records list and run the bookmarklet.',
     [Language.ko_KR]: '사용법: 최근 플레이 목록 화면에 들어간 후 북마크를 실행하세요.',
   },
   screenshotUrl: './screenshots/recent-play-summary-20200704.png',
 };
 
-export const ratingAnalyzer: Bookmarklet = {
+const analyzeSelfRating: Bookmarklet = {
   id: 'analyzeSelfRating',
   itemTitleByLang: {
     [Language.zh_TW]: '分析自己 DX Rating',
@@ -116,13 +116,13 @@ export const ratingAnalyzer: Bookmarklet = {
   howToByLang: {
     [Language.zh_TW]:
       '使用方式：於 maimai NET 首頁或個人檔案頁面執行。執行時會開新分頁，載入成績並進行分析。',
-    [Language.en_US]: 'Usage: Execute the bookmarklet on maimai NET home page or player data page.',
+    [Language.en_US]: 'Usage: Run the bookmarklet on maimai NET home page or player data page.',
     [Language.ko_KR]: '사용법: maimai NET 메인 화면 혹은 player data 화면에서 북마크를 실행하세요.',
   },
   screenshotUrl: './screenshots/rating-analyzer-20200702.png',
 };
 
-export const analyzeFriendRating: Bookmarklet = {
+const analyzeFriendRating: Bookmarklet = {
   id: 'analyzeFriendRating',
   itemTitleByLang: {
     [Language.zh_TW]: '分析好友 DX Rating',
@@ -138,14 +138,14 @@ export const analyzeFriendRating: Bookmarklet = {
     [Language.zh_TW]:
       '使用方式：於朋友清單頁面，先將想分析的好友加入最愛（ADD to FAVORITE），再執行書籤。設成最愛的好友檔案中會出現「分析 Rating」的連結，點擊後會分析該玩家的 R 值。',
     [Language.en_US]:
-      'Usage: Open friend list. Add the friend you want to analyze to FAVORITE. Execute the bookmarklet. There will have an "Analyze Rating" link for each favorite friend. Click on one of the links to analyze rating for that player.',
+      'Usage: Open friend list. Add the friend you want to analyze to FAVORITE. Run the bookmarklet. There will have an "Analyze Rating" link for each favorite friend. Click on one of the links to analyze rating for that player.',
     [Language.ko_KR]:
       '사용법: 친구 목록 화면에 들어가서 분석하고 싶은 친구를 즐겨찾기에 등록하고 북마크를 실행합니다. 즐겨찾기에 등록된 친구는 "레이팅 분석하기" 링크가 표시 될 것입니다. 해당 링크를 클릭하면 친구의 레이팅을 분석 할 수 있습니다.',
   },
   screenshotUrl: './screenshots/analyze-friend-rating-20200725.png',
 };
 
-export const scoreDownloader: Bookmarklet = {
+const scoreDownloader: Bookmarklet = {
   id: 'scoreDownloader',
   itemTitleByLang: {
     [Language.zh_TW]: '下載所有歌曲成績',
@@ -160,7 +160,7 @@ export const scoreDownloader: Bookmarklet = {
   },
   howToByLang: {
     [Language.en_US]:
-      'Usage: Execute the bookmarklet on maimai NET home page. After several seconds, a "Copy" button will appear on screen. Click the Copy button to copy scores. You can paste them in Excel or Google Sheets.',
+      'Usage: Run the bookmarklet on maimai NET home page. After several seconds, a "Copy" button will appear on screen. Click the Copy button to copy scores. You can paste them in Excel or Google Sheets.',
 
     [Language.zh_TW]:
       '使用方式：於 maimai NET 首頁執行。執行完後點下畫面上的「複製」按鈕就能複製所有成績。複製後可貼到 Excel 或是 Google 試算表。',
@@ -171,7 +171,7 @@ export const scoreDownloader: Bookmarklet = {
   screenshotUrl: './screenshots/score-download-20200630.png',
 };
 
-export const albumDownloadHelper: Bookmarklet = {
+const albumDownloadHelper: Bookmarklet = {
   id: 'albumDownloadHelper',
   itemTitleByLang: {
     [Language.zh_TW]: '相簿照片命名',
@@ -190,9 +190,19 @@ export const albumDownloadHelper: Bookmarklet = {
     [Language.zh_TW]:
       '使用方式：進入 PHOTOS 頁面後執行書籤，執行完後點擊想要下載的照片，就能存到手機或電腦上。',
     [Language.en_US]:
-      'Usage: Open PHOTOS page and execute this bookmarklet. Photos on the page will be clickable and have proper filenames.',
+      'Usage: Open PHOTOS page and run this bookmarklet. Photos on the page will be clickable and have proper filenames.',
     [Language.ko_KR]:
       '사용법: PHOTOS 페이지를 열고 북마크를 실행하세요. 해당 페이지의 사진을 클릭하고 다운로드 받을 수 있게 됩니다.',
   },
   screenshotUrl: './screenshots/album-download-helper-20210216.png',
 };
+
+export const ALL_BOOKMARKLETS = [
+  analyzeSelfRating,
+  analyzeFriendRating,
+  scoreSorter,
+  recentPlaySummary,
+  albumDownloadHelper,
+  convertDxToFinale,
+  scoreDownloader,
+];
