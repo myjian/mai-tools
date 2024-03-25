76c729b7e0088209c2825e153bd0a76ae86ebea2
Trin Beam <162117433+TrinityBeam@users.noreply.github.com>
2024-03-25 19:42:32 +1100

    Rating calc: CSS fix on +Rating Column

--- src/rating-calculator/components/ChartRecordDataRow.tsx
+++ src/rating-calculator/components/ChartRecordDataRow.tsx
@@ -67,7 +67,7 @@ export const ChartRecordDataRow = React.memo((props: Props) => {
           return record.nextRanks
             ? Array.from(record.nextRanks.values()).map((r, idx) => (
                 <div key={idx}>{Math.floor(record.level*r.rank.factor*(r.rank.minAchv/100)).toFixed(0)}
-                  {' '}(+{r.minRt.toFixed(0)})</div>
+                  (+{r.minRt.toFixed(0)})</div>
               ))
             : '';
         case ColumnType.RATING:
